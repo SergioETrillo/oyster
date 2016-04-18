b4830c61cd00aa1b8e67725b2d070858375449e2
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-18 23:07:28 +0100

    add step 10 touch out deduct moved to private and test refactor

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
 
   MAX_BALANCE = 90
-  ERR_MAX_BALANCE = "max_balance can't be >£90".freeze
+  MIN_CHARGE = 1
+  ERR_MAX_BALANCE = "Maximum balance exceeded".freeze
+  ERR_NO_MONEY = "Insufficient balance to touch in".freeze
   attr_reader :balance
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -13,8 +16,25 @@ class Oystercard
     @balance += amount
   end
 
+  def in_journey?
+    in_journey
+  end
+
+  def touch_in
+    raise ERR_NO_MONEY if balance < MIN_CHARGE
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+    deduct(MIN_CHARGE)
+  end
+
+  private
+
+  attr_reader :in_journey
+
   def deduct(fare)
     @balance -= fare
   end
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,20 +15,52 @@ describe Oystercard do
       expect(subject.balance).to eq 43
       #expect{ subject.top_up 43 }.to change{ subject.balance }.by 43
     end
+  end
 
-    context "maximum limit of £90 on balances" do
-      it "raises error when top_up exceeds max balance " do
-        subject.top_up(Oystercard::MAX_BALANCE)
-        error_max = Oystercard::ERR_MAX_BALANCE
-        expect{ subject.top_up(1) }.to raise_error error_max
-      end
-    end
-
-    context "deduct the travel fare" do
-      it "subtracts the cost of fare and updates balance" do
-        subject.top_up(20)
-        expect {subject.deduct 5.50}.to change{ subject.balance }.by -5.50
-      end
+  context "maximum limit of £90 on balances" do
+    it "raises error when top_up exceeds max balance " do
+      subject.top_up(Oystercard::MAX_BALANCE)
+      error_max = Oystercard::ERR_MAX_BALANCE
+      expect{ subject.top_up(1) }.to raise_error error_max
     end
   end
+
+  # context "deduct the travel fare" do
+  #   it "subtracts the cost of fare and updates balance" do
+  #     subject.top_up(20)
+  #     expect { subject.touch_out }.to change{ subject.balance }.by -Oystercard::MIN_CHARGE
+  #   end
+  # end
+
+  context "touch in and out" do
+    before { subject.top_up Oystercard::MAX_BALANCE }
+
+    it "when initialized card is not in journey" do
+      expect(subject.in_journey?).to be false
+    end
+
+    it "when touch-in card is in-journey" do
+      subject.touch_in
+      expect(subject.in_journey?).to be true
+    end
+
+    it "when touch in then out card is not in-journey" do
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey?).to be false
+    end
+  end
+
+  context "minimum balance £1" do
+    it "raises error if balance less than 1 when touch-in" do
+      expect { subject.touch_in}.to raise_error Oystercard::ERR_NO_MONEY
+    end
+  end
+
+  context "paying for journeys" do
+    it "deducts the right amount when touch out" do
+      expect{ subject.touch_out }.to change{ subject.balance }.by -Oystercard::MIN_CHARGE
+    end
+  end
+
 end
