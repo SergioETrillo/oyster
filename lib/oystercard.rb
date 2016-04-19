9eb378a9227cd39c2be5e06aa51c59f8eae5e93b
rahulrama <rahulrama@gmail.com>
2016-04-19 19:03:11 +0100

    Step 12: Journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,13 @@ class Oystercard
 	MAXIMUM_BALANCE = 90
 	MINIMUM_FARE = 1
 
-	attr_reader :balance, :starting_station
+	attr_reader :balance, :starting_station, :ending_station, :history
 
   def initialize
     @balance = 0
-   
-  end
+		@starting_station = nil
+		@history = {}
+	end
 
   def top_up(money)
   	raise "maximum balance of #{MAXIMUM_BALANCE} exceeded" if money + balance > MAXIMUM_BALANCE
@@ -15,17 +16,20 @@ class Oystercard
   end
 
   def in_journey?
-  	!!starting_station 
+  	!!starting_station
   end
 
-  def touch_in(station)
+  def touch_in(starting_station)
 		raise "Insufficient balance for journey" if balance < MINIMUM_FARE
-  	@starting_station = station
+  	@starting_station = starting_station
+		@history[:starting_station] = starting_station
   end
 
-  def touch_out
+  def touch_out(ending_station)
 		deduct(MINIMUM_FARE)
 		@starting_station = nil
+		@ending_station = ending_station
+		@history[:ending_station] = ending_station
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,61 +5,72 @@ describe Oystercard do
   	subject(:oystercard){ described_class.new }
     let (:card_max) { Oystercard::MAXIMUM_BALANCE }
     let (:min_fare) { Oystercard::MINIMUM_FARE }
-    let (:station) {double :station }
+    let (:starting_station) { double :station }
+    let (:ending_station) { double :station }
+    let (:journey) { {starting_station: starting_station, ending_station: ending_station} }
 
     describe "#initialization" do
     	it 'has a default balance of 0 on initialization' do
-    	  expect(oystercard.balance).to eq 0
+        expect(oystercard.balance).to eq 0
   	  end
-
 	    it "is not in journey, when card is issued" do
 	 	    expect(oystercard).not_to be_in_journey
 	    end
+      it 'has an empty history when card is issued' do
+        expect(oystercard.history).to be_empty
+      end
     end
 
-  	it 'tops up the balance' do
-  		expect{ oystercard.top_up(card_max) }.to change{oystercard.balance}.by card_max
+    describe '#top_up' do
+      it 'tops up the balance' do
+        expect{ oystercard.top_up(card_max) }.to change{oystercard.balance}.by card_max
+  	  end
+      it "raises an error when the maximum balance is exceeded" do
+  		  oystercard.top_up(card_max)
+  		  expect {oystercard.top_up(1)}.to raise_error "maximum balance of #{card_max} exceeded"
+	    end
+    end
+
+  context 'when in journey' do
+    before(:each) do
+      oystercard.top_up(card_max)
+      oystercard.touch_in(starting_station)
+    end
+    it "prevents journey if oystercard doesn't have minimum balance" do #should we consider moving this elsewhere?
+      oystercard.top_up(-card_max)
+      expect { oystercard.touch_in(starting_station) }.to raise_error "Insufficient balance for journey"
+    end
+    it "touches in and starts journey" do
+  		expect(oystercard).to be_in_journey
   	end
-
-  	it "raises an error when the maximum balance is exceeded" do
-  		oystercard.top_up(card_max)
-  		expect {oystercard.top_up(1)}.to raise_error "maximum balance of #{card_max} exceeded"
-	end
-
-  it "prevents journey if oystercard doesn't have minimum balance" do
-    expect { oystercard.touch_in(station) }.to raise_error "Insufficient balance for journey"
+    it "remembers the entry station" do
+      expect(oystercard.starting_station).to eq starting_station
+    end
   end
 
-  it "remembers a starting point station" do
-    oystercard.top_up(card_max)
-    oystercard.touch_in(station)
-    expect(oystercard.starting_station).to eq station 
-  end  
-
-	it "touches in and starts journey" do
-    oystercard.top_up(card_max)
-    oystercard.touch_in(station) 
-		expect(oystercard).to be_in_journey
-	end
-
-  it "clears a starting point station" do
-    oystercard.top_up(card_max)
-    oystercard.touch_in(station)
-    oystercard.touch_out
-    expect(oystercard.starting_station).to be nil
+  context 'when journey is complete' do
+    before do
+      oystercard.top_up(card_max)
+      oystercard.touch_in(starting_station)
+    end
+    it "touches out and ends journey" do
+  		oystercard.touch_out(ending_station)
+  		expect(oystercard).not_to be_in_journey
+  	end
+    it "deducts the correct journey fare from my card when touching out" do
+      expect { oystercard.touch_out(ending_station) }.to change { oystercard.balance }.by -(min_fare)
+    end
+    it "remembers the exit station" do
+      oystercard.touch_out(ending_station)
+      expect(oystercard.ending_station).to eq ending_station
+    end
+    it "stores a journey" do
+      oystercard.touch_out(ending_station)
+      expect(oystercard.history).to include journey
+    end
+    it "clears the starting station for the next journey" do #remember to update for multiple journeys
+      oystercard.touch_out(ending_station)
+      expect(oystercard.starting_station).to be nil
+    end
   end
-
-	it "touches out and ends journey" do
-    oystercard.top_up(card_max)
-    oystercard.touch_in(station) 
-		oystercard.touch_out
-		expect(oystercard).not_to be_in_journey
-	end
-
-  it 'deducts the correct journey fare from my card when touching out' do
-    oystercard.top_up(card_max)
-    oystercard.touch_in(station) 
-    expect { oystercard.touch_out }.to change { oystercard.balance }.by -(min_fare)
-  end
-
 end
